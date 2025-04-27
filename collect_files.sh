fcc8108ee763af2da4a83a217105ae232a15d928
root <root@zhicxwwbdg.local>
2025-04-27 22:41:54 +0000

    e

--- collect_files.sh
+++ collect_files.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 input_dir="$1"
 output_dir="$2"
-mkdir "$output_dir"
-find "$input_dir" -type f -exec cp {} "$output_dir" \;
+max_deth=0
+if [ "$3" == "--max_depth" ] && [ -n "$4" ]; then
+    max_depth="$4"
+fi
+python3 collect_files.py "$input_dir" "$output_dir" "$max_depth"
