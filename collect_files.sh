dc6f75169a05f104af120083de70115658b7fd93
root <root@zhicxwwbdg.local>
2025-04-28 02:38:48 +0000

    zzz

--- collect_files.sh
+++ collect_files.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 input_dir="$1"
 output_dir="$2"
-max_deth=0
-if [ "$3" == "--max_depth" ] && [ -n "$4" ]; then
-    max_depth="$4"
-fi
-python3 collect_files.py "$input_dir" "$output_dir" "$max_depth"
+mkdir -p "$output_dir"
+counter=1
+for filepath in $(find "$input_dir" -type f); do
+  filename=$(basename "$filepath")
+  if [ -e "$output_dir/$filename" ]; then
+    name="${filename%.*}"
+    ext="${filename##*.}"
+    if [ "$name" == "$ext" ]; then
+      new_filename="${name}_${counter}"
+    else
+      new_filename="${name}_${counter}.${ext}"
+    fi
+
+    cp "$filepath" "$output_dir/$new_filename"
+    counter=$((counter + 1))
+  else
+    cp "$filepath" "$output_dir/$filename"
+  fi
+done
